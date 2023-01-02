2e536ee887d96b7e03fe9ed8294a8959a7560209
Yuliya Tonkikh <uliya_i@bk.ru>
2023-01-02 13:57:36 +0700

    first commit

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String phone;
+    public String passport;
+
+
+    public boolean subscription = true;
+    public FormDate birthday;
+
+
+}
